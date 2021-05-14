bd5fa15531984ceb72cda9e141ac1831f410041f
wk1093 <wyattk1093@gmail.com>
2021-05-14 12:19:46 -0700

    Game Loop

--- app/src/main/java/tk/wyattk1093/firstproject/MainActivity.java
+++ app/src/main/java/tk/wyattk1093/firstproject/MainActivity.java
@@ -1,14 +1,27 @@
 package tk.wyattk1093.firstproject;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+/**
+ * MainActivity is the entry point.
+ */
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        Window window = getWindow();
+        window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+        );
+
+        // Set content view to game
+        setContentView(new Game(this));
     }
 }
